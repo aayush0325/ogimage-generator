83688a1447602143cea3ba5dbc16f63d0d7ae3da
aayush0325 <ayush25khanna@gmail.com>
2024-08-05 08:41:22 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import LandingPage from './pages/landing'
+import PostPage from './pages/post'
+
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },{
+    path: "/post",
+    element: <PostPage />,
+  }
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
