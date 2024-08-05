7707194be3b142d229adf76e4d7b82e6257d1727
aayush0325 <ayush25khanna@gmail.com>
2024-08-06 00:15:19 +0530

    post design complete

--- src/pages/viewpost.tsx
+++ src/pages/viewpost.tsx
@@ -1,6 +1,10 @@
 import { useLocation } from "react-router-dom";
+import { UserButton } from "@clerk/clerk-react";
+import { useClerk } from "@clerk/clerk-react";
 
 export default function ViewPost() {
+    const { user } = useClerk();
+    const fullName = user?.fullName;
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
 
@@ -9,10 +13,32 @@ export default function ViewPost() {
     const content: string | null = queryParams.get('content');
 
     return (
-        <div>
-            <img src={imageURL ?? ""} />
-            <h1>{title ?? "No title"}</h1>
-            <p>{content ?? "No content"}</p>
+        <div className="bg-gray-900 h-screen w-screen flex flex-col items-center p-6">
+            <div className="bg-white text-black rounded-lg shadow-lg p-3 max-w-4xl flex flex-col sm:flex-row">
+                <div className="max-h-80 min-w-fit flex justify-center items-center rounded overflow-hidden border-black border-2">
+                    {imageURL &&
+                        <img
+                            src={imageURL}
+                            className="w-full h-auto object-contain cursor-pointer"
+                            loading="lazy"
+                        />
+                    }
+                </div>
+                <div className="py-2 sm:px-2 flex flex-col justify-between ml-3 flex-grow">
+                    <div className="flex items-center justify-between mb-2">
+                        <div className="text-2xl font-bold mr-4">
+                            {title ?? "Untitled Post"}
+                        </div>
+                        <UserButton />
+                    </div>
+                    <div className="mt-2 whitespace-pre-wrap flex-grow">
+                        {content ?? "No content available."}
+                    </div>
+                    <div className="mt-auto self-end text-sm text-gray-600">
+                        - {fullName}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
